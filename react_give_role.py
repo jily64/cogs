c3ebc61e909903357bca83c4204d5ff0a5c71369
jily64 <108062664+jily64@users.noreply.github.com>
2023-12-12 13:17:19 +0300

    Commit

--- react_give_role.py
+++ react_give_role.py
@@ -13,55 +13,57 @@ class rk(commands.Cog):
     async def react(self, interaction: discord.Interaction, message_id: str, role_id: discord.Role, emoji_name: str):
         message_id = int(message_id)
         role_id = role_id.id
-        with open(f"servers/{interaction.guild.id}.json", "r", encoding="utf-8") as f:
-            datas = json.load(f)
-            f.close()
-        try:
-            if 'rgr' not in datas:
+        if interaction.user.guild_permissions.administrator:
+            with open(f"servers/{interaction.guild.id}.json", "r", encoding="utf-8") as f:
+                datas = json.load(f)
+                f.close()
+            try:
+                if 'rgr' not in datas:
+                    datas["rgr"] = {}
+            except:
                 datas["rgr"] = {}
-        except:
-            datas["rgr"] = {}
 
-        data = datas["rgr"]
+            data = datas["rgr"]
 
-        try:
-            message = data[str(message_id)]
-            data[str(message_id)][emoji_name] = {
-                "role": int(role_id)
-            }
-
-        except:
-            message = {
-                emoji_name: {
+            try:
+                message = data[str(message_id)]
+                data[str(message_id)][emoji_name] = {
                     "role": int(role_id)
                 }
-            }
-            data[str(message_id)] = message
-        datas["rgr"] = data
-        print(emoji_name)
-        with open(f"servers/{interaction.guild.id}.json", "w") as f:
-            json.dump(datas, f, indent=4)
-        channel = self.bot.get_channel(interaction.channel.id)
-        message = await channel.fetch_message(int(message_id))
-        await message.add_reaction(emoji_name)
-        await interaction.response.send_message("Успешно добавлены роли на сообщение", ephemeral=True)
+
+            except:
+                message = {
+                    emoji_name: {
+                        "role": int(role_id)
+                    }
+                }
+                data[str(message_id)] = message
+            datas["rgr"] = data
+            print(emoji_name)
+            with open(f"servers/{interaction.guild.id}.json", "w") as f:
+                json.dump(datas, f, indent=4)
+            channel = self.bot.get_channel(interaction.channel.id)
+            message = await channel.fetch_message(int(message_id))
+            await message.add_reaction(emoji_name)
+            await interaction.response.send_message("Успешно добавлены роли на сообщение", ephemeral=True)
 
     @app_commands.command(name="очистить_сообщение", description="убрать все привязанные роли к сообщению")
     async def clear(self, interaction: discord.Interaction, message_id: str):
-        message_id = int(message_id)
-        with open(f"servers/{interaction.guild.id}.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-            f.close()
-        if str(message_id) in data["rgr"]:
-            data["rgr"].pop(str(message_id))
-            message = await interaction.channel.fetch_message(message_id)
-            await message.clear_reactions()
-            await interaction.response.send_message("Теперь сообщение чистенькое! :З", ephemeral=True)
-        else:
-            await interaction.response.send_message("Я не нашел такого сообщения(", ephemeral=True)
+        if interaction.user.guild_permissions.administrator:
+            message_id = int(message_id)
+            with open(f"servers/{interaction.guild.id}.json", "r", encoding="utf-8") as f:
+                data = json.load(f)
+                f.close()
+            if str(message_id) in data["rgr"]:
+                data["rgr"].pop(str(message_id))
+                message = await interaction.channel.fetch_message(message_id)
+                await message.clear_reactions()
+                await interaction.response.send_message("Теперь сообщение чистенькое! :З", ephemeral=True)
+            else:
+                await interaction.response.send_message("Я не нашел такого сообщения(", ephemeral=True)
 
-        with open(f"servers/{interaction.guild.id}.json", "w") as f:
-            json.dump(data, f, indent=4)
+            with open(f"servers/{interaction.guild.id}.json", "w") as f:
+                json.dump(data, f, indent=4)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
